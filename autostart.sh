a49eea13b0394ee74ad0e4f021f11d2e6fd6bd24
AntSunrise <antsunrise83@gmail.com>
2015-02-25 14:08:58 -0500

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,36 +1,42 @@
 ## tint panel
 (sleep 2s && tint2) &
- 
- 
+  
+  
 ## Enable power management
 xfce4-power-manager &
- 
+  
 ## Start Thunar Daemon
 thunar --daemon &
- 
- 
+  
+  
 ## Start xscreensaver
 xscreensaver -no-splash &
- 
+  
+  
 ## Set keyboard settings - 250 ms delay and 25 cps (characters per second) repeat rate.
 ## Adjust the values according to your preferances.
 xset r rate 250 25 &
- 
+  
 ## nitrogen
 (nitrogen --restore) &&
- 
-## mixer
- (pnmixer) &
- 
+  
+  
 ## Volume keys daemon
 xfce4-volumed &
- 
- 
+  
+  
 ## Start Gnome Policy Kit
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
-## X11 Compostier
-(sleep 2s && compton) &
+## Compoister
+compton &
 
-## terminal
-rxvt-unicode &
+## polkit
+polkit-gnome &
+
+## Octopi-notofier
+(sleep 5s && octopi-notifier) &
+
+## MPD
+(sleep 3s && mpd) &
+
